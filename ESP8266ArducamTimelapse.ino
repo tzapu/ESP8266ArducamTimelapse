6fa8df9beeb4f2ffbf44d244509142d99c5d0ec3
Alex Tudorache <alex@tzapu.com>
2016-03-09 19:51:14 +0200

    time tracking and drift tracking

--- ESP8266ArducamTimelapse.ino
+++ ESP8266ArducamTimelapse.ino
@@ -15,12 +15,50 @@ extern "C" {
 }
 
 typedef struct {
-  byte markerFlag;
-  byte counter;
+  int           verification;
+  int           cycles;
   unsigned long sleepTime;
+  time_t        lastTimestamp;
+  int           driftCalibration;
 } rtcStore __attribute__((aligned(4))); // not sure if the aligned stuff is necessary, some posts suggest it is for RTC memory?
 
 rtcStore rtcMem;
+
+#define RTC_VALIDATION 4579
+#define DRIFT_CALIBRATION 200
+#define CALIBRATION_CYCLES 5
+
+#define MAX_SLEEP_INTERVAL_US (1000 * 1000 * 30)
+
+time_t currentTimestamp;
+
+void printRTCMem() {
+  Serial.println("RTC Mem");
+  Serial.print("last timestamp: ");
+  Serial.print(ctime(&rtcMem.lastTimestamp));
+  Serial.print("sleep time: ");
+  Serial.println(rtcMem.sleepTime);
+  Serial.print("verification: ");
+  Serial.println(rtcMem.verification);
+  Serial.print("current time: ");
+  Serial.print(ctime(&currentTimestamp));
+  Serial.print("cycles: ");
+  Serial.println(rtcMem.cycles);
+  Serial.print("drift calibration: ");
+  Serial.println(rtcMem.driftCalibration);
+}
+
+void gotoSleep() {
+  rtcMem.verification = RTC_VALIDATION;
+  rtcMem.cycles++;
+  rtcMem.lastTimestamp = currentTimestamp + (millis() + rtcMem.driftCalibration) / 1000;
+  rtcMem.sleepTime = MAX_SLEEP_INTERVAL_US / 1000 / 1000;
+  system_rtc_mem_write(65, &rtcMem, sizeof(rtcMem));
+  printRTCMem();
+  ESP.deepSleep(MAX_SLEEP_INTERVAL_US, WAKE_RF_DEFAULT);
+  delay(100);
+}
+
 
 void setup() {
   wifi_station_disconnect();
@@ -29,39 +67,44 @@ void setup() {
   wifi_fpm_open();
   wifi_fpm_do_sleep(0xFFFFFFF);
 
+
+
   Serial.setDebugOutput(true);
   Serial.begin(115200);
   delay(100);
   Serial.println();
   Serial.println();
   Serial.println("Start");
+  //Serial.println(system_get_rtc_time());
   Serial.println(ESP.getResetReason());
-  int val = analogRead(A0);
-  Serial.println(val);
-  delay(500);
+  //Serial.println(ESP.getResetInfo());
+
+  system_rtc_mem_read(65, &rtcMem, sizeof(rtcMem));
+  printRTCMem();
+  if (rtcMem.verification != RTC_VALIDATION) {
+    Serial.println("invalid values");
+    rtcMem.cycles = 0;
+    rtcMem.sleepTime = 0;
+    rtcMem.lastTimestamp = 0;
+    rtcMem.driftCalibration = 200; //ms
+  }
+  currentTimestamp = rtcMem.lastTimestamp + rtcMem.sleepTime;
+  printRTCMem();
+
+
+
   int a = analogRead(A0);
+  Serial.println(a);
   if (a > 512 ) {
     Serial.println("Normal start");
-    time_t now = time(nullptr);
-    if (!now) {
-      Serial.println("We have no time");
+    time_t now = currentTimestamp;
+    //if no time or resync cycle
+    if (!now || rtcMem.cycles % CALIBRATION_CYCLES == 0) {
+      Serial.println("We have no time saved or it s s sync cycle");
 
-      /*
-        Serial.println("deep sleep fix");
-        wifi_fpm_do_wakeup();
-        wifi_fpm_close();
-
-        wifi_set_opmode(STATION_MODE);
-        wifi_station_connect();
-        delay(1);
-      */
       Serial.println("deep sleep fix");
       wifi_fpm_do_wakeup();
       wifi_fpm_close();
-
-      Serial.println("Reconnecting");
-      //wifi_set_opmode(STATION_MODE);
-      //wifi_station_connect();
 
       WiFiManager wifi;
       if (!wifi.autoConnect("Cashula")) {
@@ -73,7 +116,19 @@ void setup() {
         Serial.print(".");
         delay(100);
       }
+      Serial.println();
       now = time(nullptr);
+      int drift = 0;
+      if (currentTimestamp != 0) {
+        drift = (now - millis() / 1000) - currentTimestamp;
+      }
+      Serial.print("DRIFT: ");
+      Serial.println(drift);
+      Serial.println((now - millis() / 1000));
+      Serial.println(currentTimestamp);
+      //correct for current elapsed runtime
+      currentTimestamp = now - millis() / 1000;
+      rtcMem.driftCalibration += (drift * 1000) / CALIBRATION_CYCLES;
     }
     Serial.println(ctime(&now));
     Serial.println(now);
@@ -117,11 +172,14 @@ void setup() {
     Serial.println("Ready");
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
+
+    //invalidate rtc
+    rtcMem.verification = 0;
+    system_rtc_mem_write(65, &rtcMem, sizeof(rtcMem));
     return;
   }
   Serial.println("go to sleep");
-  ESP.deepSleep(1000 * 1000 * 30, WAKE_RF_DEFAULT);
-  delay(1000);
+  gotoSleep();
 }
 
 void loop() {
